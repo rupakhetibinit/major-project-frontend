0f77986127d070178f8e1e81b1cbd90989cdd645
Binit Rupakheti <crusierponder@protonmail.com>
2023-02-18 17:24:13 +0545

    chore: add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import { QueryClientProvider, QueryClient } from "react-query";
 import "../styles/globals.css";
-
+const queryClient = new QueryClient();
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
